f496000535b0f75cc2930b38f3c062bb500d2c33
andycheng <ccjeng@gmail.com>
2015-10-07 11:19:07 +0800

    init

---
+++ sync.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+from parse_rest.connection import register
+from parse_rest.datatypes import Object, GeoPoint
+
+import json
+import urllib2
+import geocoder
+
+APPLICATION_ID = "nxkxfDhpFQBXOReTPFIPhGIaYowmT5uuscj3w3Kb"
+REST_API_KEY = "6UXzStOsl61BtH2Y905HYkNDeObZ4iQqz0Pu1TRO"
+
+url = 'http://data.ntpc.gov.tw/od/data/api/28AB4122-60E1-4065-98E5-ABCCB69AACA6?$format=json'
+
+
+register(APPLICATION_ID, REST_API_KEY)
+
+class RealTime_STG2(Object):
+    pass
+
+
+response = urllib2.urlopen(url).read().decode('utf8')
+
+data = json.loads(response)
+
+items = data
+
+#delete data
+realtime = RealTime_STG2.Query.all()
+realtime.limit(1000)
+for rt in realtime:
+	rt.delete()
+
+
+#import data
+for item in items:
+	g = geocoder.google(item['location'])
+	trashcanItem = RealTime_STG2(lineid=item['lineid'], address=item['location'], carno=item['car'], cartime=item['time'])
+	trashcanItem.location = GeoPoint(latitude=float(g.lat), longitude=float(g.lng))
+	trashcanItem.save()
+
+
