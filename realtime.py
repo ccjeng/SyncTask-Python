af080b5e18a916663ab9952d297474196ef84acd
andycheng <ccjeng@gmail.com>
2016-02-16 10:50:25 +0800

    add logentries

--- realtime.py
+++ realtime.py
@@ -7,10 +7,17 @@ import geocoder
 import requests
 from firebase import firebase
 from apscheduler.schedulers.blocking import BlockingScheduler
+from logentries import LogentriesHandler
+import logging
 
 firebase = firebase.FirebaseApplication(
     'https://tptrashcarrealtime.firebaseio.com/', None)
 stgTable = 'STG_Heroku'
+
+# logentries
+log = logging.getLogger('logentries')
+log.setLevel(logging.INFO)
+log.addHandler(LogentriesHandler('4b58a6f6-1bb8-4275-b39e-3c64ca5266e4'))
 
 
 def main():
@@ -24,6 +31,7 @@ def main():
     firebase.delete(stgTable, None)
 
     print('count = ' + str(len(items)))
+    log.info('count = ' + str(len(items)))
 
     for item in items:
         g = geocoder.google(item['location'])
@@ -33,17 +41,20 @@ def main():
             result = firebase.post(stgTable, data)
         else:
             print(item['lineid'] + ',' + str(g.lat) + ',' + str(g.lng))
+            log.info(item['lineid'] + ',' + str(g.lat) + ',' + str(g.lng))
 
     # Copy to PROD
     print('Copy to PROD')
+    log.info('Copy to PROD')
     firebase.delete('/PROD', None)
     stgResults = firebase.get(stgTable, None)
     firebase.patch('/PROD', stgResults)
     print('Done')
+    log.info('Done')
+
 
 sched = BlockingScheduler()
 logging.basicConfig()
-
 
 @sched.scheduled_job('interval', minutes=5)
 def timed_job():
@@ -51,6 +62,7 @@ def timed_job():
         main()
     except Exception, e:
         print(e)
+        log.warn(e)
     else:
         pass
 
