78b9288569537b35f26ed7b621f0e527dde3ced5
andycheng <ccjeng@gmail.com>
2016-02-16 11:03:05 +0800

    no message

--- realtime.py
+++ realtime.py
@@ -7,18 +7,10 @@ import geocoder
 import requests
 from firebase import firebase
 from apscheduler.schedulers.blocking import BlockingScheduler
-from logentries import LogentriesHandler
-import logging
 
 firebase = firebase.FirebaseApplication(
     'https://tptrashcarrealtime.firebaseio.com/', None)
 stgTable = 'STG_Heroku'
-
-# logentries
-log = logging.getLogger('logentries')
-log.setLevel(logging.INFO)
-log.addHandler(LogentriesHandler('4b58a6f6-1bb8-4275-b39e-3c64ca5266e4'))
-
 
 def main():
 
@@ -40,17 +32,15 @@ def main():
                     'time': item['time'], 'lat': g.lat, 'lng': g.lng}
             result = firebase.post(stgTable, data)
         else:
-            print(item['lineid'] + ',' + str(g.lat) + ',' + str(g.lng))
-            log.info(item['lineid'] + ',' + str(g.lat) + ',' + str(g.lng))
+            print(item['location'] + ',' + str(g.lat) + ',' + str(g.lng))
 
     # Copy to PROD
     print('Copy to PROD')
-    log.info('Copy to PROD')
     firebase.delete('/PROD', None)
     stgResults = firebase.get(stgTable, None)
     firebase.patch('/PROD', stgResults)
     print('Done')
-    log.info('Done')
+
 
 
 sched = BlockingScheduler()
@@ -62,8 +52,6 @@ def timed_job():
         main()
     except Exception, e:
         print(e)
-        log.warn(e)
-    else:
-        pass
 
 sched.start()
+
